d5ac9416456e582cf779e3d09d914875bf41e2f1
Edward Terry <edward.william.terry@gmail.com>
2018-04-28 18:32:21 -0400

    controller working with bag

--- reefbot-controller/bin/follow_ball.py
+++ reefbot-controller/bin/follow_ball.py
@@ -9,19 +9,15 @@ from reefbot_msgs.msg import RobotStatus
 import time
 import math
 from robot_api import *
+import numpy as np
 
 # set the *_max values below to control max values
 # currenty left and right are scaled.
 # lights and vertical are the actual values used
-left_motor_max = 10.0 * 0.2
-right_motor_max = 10.0 * 0.2
-lights_max = 80.0
-vertical_motor_max = 70.0 * 0.05
-
-# horiz_pixels = 540
-# vert_pixels = 410
-
-global last_ball_msg
+# left_motor_max = 0 # 10.0 * 0.2
+# right_motor_max = 0 #10.0 * 0.2
+# lights_max = 80.0
+# vertical_motor_max = 0 # 70.0 * 0.05
 
 left_motor_cmd = 0
 right_motor_cmd = 0
@@ -30,43 +26,30 @@ lights_max_cmd = 0
 
 detection_data = Point()
 
-# heading_setpoint = 0
-# vert_setpoint = 0
-# forward_setpoint = 0
-
-# vert_kP = vertical_motor_max / 2.0  # max vertical thrust when 2 meters above / below
-# forward_kP = left_motor_max / 5.0  # max forward thrust when 5 meters away
-# heading_kP = left_motor_max / (3.14)  # max turning when 180 deg off
-
 autonomous_mode = False
 
+max_cmds = {'left': 2.0, 'right': 2.0, 'vert': 3.5, 'lights': 8.0}
 
-
-# ball_msg_timeout = 1.0 # s
-global ball_msg_received_time
 
 class Controller:
-    def __init__(self, max_motor_cmds):
-        self.max_motor_cmds = {'left': max_motor_cmds['left'],  \
-                               'right': max_motor_cmds['right'],  \
-                               'vert': max_motor_cmds['vert']}
+    def __init__(self, max_cmds):
+        self.max_motor_cmds = {'left': max_cmds['left'],  \
+                               'right': max_cmds['right'],  \
+                               'vert': max_cmds['vert']}
         self.motor_cmds =   {'left': 0.0,   'right': 0.0,   'vert': 0.0}
         self.ball =         {'cx': 0.0,     'cy': 0.0,      'dist': 0.0} # ball position and size in frame
+        self.update =       {'cx': 0.0,     'cy': 0.0,      'dist': 0.0} # ball position and size in frame
         self.gains =        {'fwd': 1.0,    'yaw': 1.0,     'vert': 1.0}
         self.errors =       {'fwd': 0.0,    'yaw': 0.0,     'vert': 0.0}
         self.desired_separation_distance = 1.0 # m
-
-        # self.setpoints = {'fwd': desired_separation_distance, 'yaw': 0.0, 'vert' = 0.0}
         self.horiz_pixels = 540.0
         self.vert_pixels = 410.0
+        self.gamma = 0.8
+        # self.ball_av = {'cx': np.zeros(1),     'cy': np.zeros(1),      'dist': np.zeros(1)}
+        # self.num_hist_elms = 5
 
     def get_motor_cmds(self):
         return self.motor_cmds['left'], self.motor_cmds['right'], self.motor_cmds['vert']
-
-    def process_detection_data(self, detection_msg):
-        self.ball['cx'], self.ball['cy'] = self.normalize_pixels(detection_msg.x, detection_msg.y)
-        self.ball['dist'] = self.get_ball_distance(detection_msg.z)
-        # self.set_setpoints()
 
 
     def get_ball_distance(self, radius):
@@ -79,61 +62,74 @@ class Controller:
         return x_norm, y_norm
 
 
-    def stay_still():
+    def ball_visible(self, data):
+        if data.x < 0 or data.y < 0 or data.z < 0: # corresponds to no detection 
+            return False
+        else:
+            return True
+
+
+    def stay_still(self):
         self.motor_cmds = self.motor_cmds.fromkeys(self.motor_cmds, 0.0) # reset all to 0
 
-    def update(self, data):
-        if data.x < 0 or data.y < 0 or data.z < 0: # corresponds to no detection 
-            self.ball['cx']     =   0.0
-            self.ball['cy']     =   0.0
-            self.ball['dist']   =   self.desired_separation_distance
-        else: 
-            self.ball['cx']     =   data.x
-            self.ball['cy']     =   data.y
-            self.ball['dist']   =   data.z      
 
-    def move(self, current_heading):
+    def update_states(self, data):
+        # update ball location
+        self.update['cx'], self.update['cy'] = self.normalize_pixels(data.x, data.y)
+        self.update['dist']                  = self.get_ball_distance(data.z)
+
+        self.ball['cx']                      = (1.0 - self.gamma) * self.ball['cx'] + self.gamma * self.update['cx']
+        self.ball['cy']                      = (1.0 - self.gamma) * self.ball['cy'] + self.gamma * self.update['cy']
+        self.ball['dist']                    = (1.0 - self.gamma) * self.ball['dist'] + self.gamma * self.update['dist']
+ 
+
+    def move(self, data, current_heading):
+        self.update_states(data)
+
         # get errors
         self.errors['fwd']  =   self.ball['dist'] - self.desired_separation_distance
         self.errors['yaw']  =   math.atan2(self.ball['cx'], self.ball['dist'])
         self.errors['vert'] =   self.ball['cy']
 
         # set motor commands
-        self.motor_cmds['vert'] = self.gains['vert'] * self.errors['vert']
-        self.motor_cmds['left'] = self.gains['fwd'] * self.errors['fwd'] + self.gains['yaw'] * self.errors['yaw']
-        self.motor_cmds['right'] = self.gains['fwd'] * self.errors['fwd'] - self.gains['yaw'] * self.errors['yaw']
+        self.motor_cmds['vert']     = self.gains['vert'] * self.errors['vert']
+        self.motor_cmds['left']     = self.gains['fwd'] * self.errors['fwd'] + self.gains['yaw'] * self.errors['yaw']
+        self.motor_cmds['right']    = self.gains['fwd'] * self.errors['fwd'] - self.gains['yaw'] * self.errors['yaw']
 
         # check against max
-        if left_motor_cmd > left_motor_max or right_motor_cmd > right_motor_max:
-            mult_factor = left_motor_max / max(left_motor_cmd, right_motor_cmd)
-            left_motor_cmd *= mult_factor
-            right_motor_cmd *= mult_factor
+        # self.motor_cmds['vert']     = min(self.max_motor_cmds['vert'], self.motor_cmds['vert'])# * np.sign(self.motor_cmds['vert'])
+        # self.motor_cmds['left']     = min(self.max_motor_cmds['left'], self.motor_cmds['left'])# * np.sign(self.motor_cmds['left'])
+        # self.motor_cmds['right']    = min(self.max_motor_cmds['right'], self.motor_cmds['right'])# * np.sign(self.motor_cmds['right'])
 
 
 
 def process_ball_detection_msg(data): # callback for ball detection
+    global detection_data
     detection_data = data
 
 
 def joy_callback(data):
+    global left_motor_cmd, right_motor_cmd, vertical_motor_cmd, lights_max_cmd
+    global max_cmds
+
     if data.buttons[0] > 0.9:
         # print('Autonomous mode commanded')
         global autonomous_mode
         autonomous_mode = True
-        lights_max_cmd = 5
+        lights_max_cmd = max_cmds['lights'] * 0.5
 
     else:    
         autonomous_mode = False
         # print('Received joystick message') 
         
-        left_motor_cmd = 0 
-        right_motor_cmd = 0 
-        vertical_motor_cmd = 0
-        lights_max_cmd = 0
+        # left_motor_cmd = 0 
+        # right_motor_cmd = 0 
+        # vertical_motor_cmd = 0
+        # lights_max_cmd = 0
 
         # map joystick inputs
-        left_motor_input = data.axes[1] # continuous in range of [-1, 1]
-        right_motor_input = data.axes[4] # continuous in range of [-1, 1]
+        left_motor_input = -data.axes[1] # continuous in range of [-1, 1]
+        right_motor_input = -data.axes[4] # continuous in range of [-1, 1]
         vertical_motor_input = 1.0-(data.axes[2]+1.0)/2.0 # continuous in range of [0, 1]
         vertical_direction = data.buttons[4] # 0 when NOT pressed, 1 when pressed
         if vertical_direction == 1: # going UP 
@@ -143,19 +139,12 @@ def joy_callback(data):
         main_thrust_reduction_factor = 1.0 # 0.2
         vertical_thrust_reduction_factor = 1.0 # 0.05
 
-        left_motor_cmd = left_motor_input * main_thrust_reduction_factor * left_motor_max
-        right_motor_cmd = right_motor_input * main_thrust_reduction_factor * right_motor_max
-        vertical_motor_cmd = vertical_motor_input * vertical_thrust_reduction_factor * vertical_motor_max * vertical_direction_cmd
-
-        global left_motor_cmd, right_motor_cmd, vertical_motor_cmd, lights_max_cmd
+        left_motor_cmd = left_motor_input * max_cmds['left']
+        right_motor_cmd = right_motor_input * max_cmds['right']
+        vertical_motor_cmd = vertical_motor_input * max_cmds['vert'] * vertical_direction_cmd
 
 
-# def within_timeout_period():
-#     if time.time()-ball_msg_received_time <ball_msg_timeout:
-#         return True
-#     else:
-#         return False
-    
+
 def listener(args):
     rospy.init_node('follow_ball')
     print "Starting Controller\n"
@@ -170,31 +159,26 @@ def listener(args):
     rospy.Subscriber("ball_detection", Point, process_ball_detection_msg, queue_size=1)
     rospy.Subscriber("joy", Joy, joy_callback, queue_size=1)
 
-    max_motor_cmds = {'left': 2.0, 'right': 2.0, 'vert': 3.5}
-
-    c = Controller(max_motor_cmds)
+    global left_motor_cmd, right_motor_cmd, vertical_motor_cmd, lights_max_cmd
+    global max_cmds
+    c = Controller(max_cmds)
 
     rate = rospy.Rate(5)  # in Hz
     while not rospy.is_shutdown():
         if autonomous_mode:
-            # print('In autonomous mode')
-        #     if within_timeout_period(): # if you've received a recent ball detection message
-        #         print("Spotted a ball!")
-            c.update(detection_data)
-            c.move(v.robotStatus.heading) # this calculates the motor commands which will be sent in the following lines
+            if c.ball_visible(detection_data):
+                c.move(detection_data, v.robotStatus.heading) # this calculates the motor commands which will be sent in the following lines
+            else:
+                c.stay_still()
             left_motor_cmd, right_motor_cmd, vertical_motor_cmd = c.get_motor_cmds()
-        #     else:
-        #         print("Where's that ball?")
-        #         stay_still()
-        # print left_motor_cmd, right_motor_cmd
+            # print c.ball
+        # left_motor_cmd = 0
         v.set_velocity(left_motor_cmd, right_motor_cmd)
         v.set_vertical(vertical_motor_cmd)
-        # global robot_heading
-        # robot_heading = v.robotStatus.heading
-        v.set_lights(lights_max_cmd)
+        # v.set_lights(lights_max_cmd)
         v.send_command()
         print "Autonomous: ", autonomous_mode
-        # print "Sending Commands Left:", left_motor_cmd, " Right:", right_motor_cmd, "Vertical:", vertical_motor_cmd, "Lights:", lights_max_cmd, ".\n"
+        print "Sending Commands Left:", left_motor_cmd, " Right:", right_motor_cmd, "Vertical:", vertical_motor_cmd, "Lights:", lights_max_cmd, ".\n"
 
         rate.sleep()
 
